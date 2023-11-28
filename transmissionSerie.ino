c66f3cf5fd2d3e94080a8e09d546e263abb3fdeb
Thomas <thomas@gmail.com>
2023-11-28 10:10:34 +0100

    ChaineDeCaracter

--- transmissionSerie.ino
+++ transmissionSerie.ino
@@ -27,22 +27,14 @@
 #define FOSC 16000000 // Clock Speed
 #define BAUD 9600
 #define MYUBRR FOSC/16/BAUD-1
+
 int flag = 0;
 int main(void)
 {
   USART_Init(MYUBRR);
   sei();
   while (1){
-    if (flag==1){
-        USART_Transmit(UDR0 + 1);
-        flag = 0;
-    }
-    _delay_ms(1);
   }
-}
-
-ISR(USART_RX_vect){
-  flag = 1;
 }
 
 void USART_puts(unsigned char *str)
@@ -62,14 +54,6 @@ while (!(UCSR0A & (1<<UDRE0)))
 UDR0 = data;
 }
 
-unsigned char USART_Receive(void)
-{
-/* Wait for data to be received */
-while (!(UCSR0A & (1<<RXC0)))
-;
-/* Get and return received data from buffer */
-return UDR0;
-}
 
 void USART_Init(unsigned int ubrr)
 {
